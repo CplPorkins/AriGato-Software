c23525d66a0279cde06ec495330d55b9602853fb
Laurentius912 <lvargas@stuy.edu>
2016-01-15 10:24:34 -0500

    added mutators/accessors to character.java

--- Character.java
+++ Character.java
@@ -3,31 +3,41 @@ public abstract class Character{
     protected int HP ;
     protected int MP;
     protected int XP;
-    protected int level;
-    protected int attack ;
-    protected int defense ;
-    protected int mana;
-    protected int strength ;
-    protected int dexterity ;
-    protected int wisdom ;
-    protected int charisma ;
-    protected int intelligence;
-    protected int constitution;
+    protected int lev;
+    protected int att;
+    protected int def;
+    protected int str;
+    protected int dex;
+    protected int wis;
+    protected int cha;
+    protected int intel;
+    protected int con;
 
-    //Should we make methods public or protected?
-    
-    public void changeHP(int damage){
-	HP-=damage;
-    }
+    //accessors
+    public int getHP() {return HP;}
+    public int getMP() {return MP;}
+    public int getXP() {return XP;}
+    public int getLev() {return lev;}
+    public int getAtt() {return att;}
+    public int getDef() {return def;}
+    public int getStr() {return str;}
+    public int getDex() {return dex;}
+    public int getWis() {return wis;}
+    public int getCha() {return cha;}
+    public int getIntel() {return intel;}
+    public int getCon() {return con;}
 
-    public void changeMP(int cost){
-	MP-=cost;
-    }
-
-    
-
-    public boolean isDefeated(){
-	return HP<=0;
-    }
-
+    //mutators
+    //REMEMBER: SET NEG VALUES TO REDUCE, POS TO INCREASE
+    public void setHP(int health) {HP += health;}
+    public void setMP(int mana) {MP += mana;}
+    public void setLev(int level) {lev += level;}
+    public void setAtt(int attack) {att += attack;}
+    public void setDef(int defense) {def += defense;}
+    public void setStr(int strength) {str += strength;}
+    public void setDex(int dexterity) {dex += dexterity;}
+    public void setWis(int wisdom) {wis += wisdom;}
+    public void setCha(int charisma) {cha += charisma;}
+    public void setIntel(int intelligence) {intel += intelligence;}
+    public void setcon(int constitution) {con += constitution;}
 }
